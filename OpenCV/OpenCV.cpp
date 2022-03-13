1b292ea787b4b408894b2d9ee8710abde1547a82
unknown <CALLIT1@student.op.ac.nz>
2022-03-13 17:48:44 +1300

    Import and add profile detect model, currently works independantly but will ideally work alongside the face detect in the future

--- OpenCV/OpenCV.cpp
+++ OpenCV/OpenCV.cpp
@@ -1,6 +1,7 @@
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include<opencv2/opencv.hpp>
 
 #include <iostream>
 #include <stdio.h>
@@ -10,25 +11,28 @@ using namespace cv;
 
 int main()
 {
-	string xmlPath = "C:/users/Boreyev/Documents/libraries/opencv/build/etc/haarcascades//haarcascade_frontalface_alt.xml";
-	string referenceImage = "D:/Desktop/bryv/image/newimg/Camera Roll/WIN_20220307_01_06_17_Pro";
+	//Add profile and frontal face detect libraries. 
+	string xmlPath1 = "C:/users/Boreyev/Documents/libraries/opencv/build/etc/haarcascades//haarcascade_frontalface_alt.xml";
+	string xmlPath2 = "C:/users/Boreyev/Documents/libraries/opencv/build/etc/haarcascades//haarcascade_profileface.xml";
 
-	double scale = 1; //Scales frame resolution (higher = quicker, 1.0 = default)
+	double scale = 3; //Scales frame resolution (higher = quicker, 1.0 = default)
 
 	//Importing cascade dependencies
-	CascadeClassifier faceCascade;
-	faceCascade.load(xmlPath);
+	CascadeClassifier faceCascade, profileCascade;
+
+	faceCascade.load(xmlPath1);
+	profileCascade.load(xmlPath2);
 
 	//Capture video frame from webcam
-	VideoCapture cap(0);
-	if (!cap.isOpened())
+	VideoCapture cam(0);
+	if (!cam.isOpened())
 		return -1;
-
+	
 	//While true:
 	for (;;)
 	{
 		Mat frame;
-		cap >> frame;
+		cam >> frame;
 
 		//Convert captured frames to grayscale. Haar Cascade face detection requires a grayscale image.
 		Mat grayscale;
@@ -39,15 +43,18 @@ int main()
 		//Create rectangle vector, Set for detectMultiScale method parameters:
 		//Image matrix, rectangle vect, scale factor, min neighbours, flags, min object size, max object size.
 		vector<Rect> faceArea;
-		faceCascade.detectMultiScale(grayscale, faceArea, 1.1, 3, 0, Size(5, 50));
-
-		//For area in faceArea: set colour-range to 255 (grayscale)
+		profileCascade.detectMultiScale(grayscale, faceArea, 1.1, 3, 0, Size(30, 30));
+		
+		//For area in faceArea: set colour to (0, 255, 0).
 		//Creates rectangle around detected face
 		for (Rect area : faceArea) {
-			Scalar drawColor = Scalar(255, 0, 0);
+			Scalar drawColor = Scalar(0, 255, 255);
 			rectangle(frame, Point(cvRound(area.x * scale), cvRound(area.y * scale)),
 				Point(cvRound((area.x + area.width - 1) * scale), cvRound((area.y + area.height - 1) * scale)), drawColor);
+			
+			putText(frame, "Face Detected", cv::Point(area.x +100, area.y +60), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 255, 0), 1.5, 8);
 		}
+
 
 		//Displays webcam frame to user.
 		imshow("Webcam", frame);
