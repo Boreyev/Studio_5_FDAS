08efdf719e0335d73c906baf63e83cb91b1e6839
unknown <CALLIT1@student.op.ac.nz>
2022-04-13 22:30:04 +1200

    Cleaned up a lot of the code by making a few functions for visual and data output.

--- main.py
+++ main.py
@@ -1,30 +1,62 @@
 from cgitb import small
-from time import sleep
 import time
 import face_recognition as fr
 import numpy as np
 import cv2
 import datetime
 
-ptime = 0 #Time = 0
 webcam = cv2.VideoCapture(0) #takes video from webcam
 font = cv2.FONT_HERSHEY_SIMPLEX #font for all writing
+ptime = 0 #Time = 0
 
 def make_480p():    #Adjusts the camera input to 480p, saves resources. CANNOT BE UPSCALED!
     webcam.set(3, 640)
     webcam.set(4, 480)
 
+def save_Face():    #Each time face is detected, save image with name and confidence level
+    return cv2.imwrite("live_dataset/"+name+"/"+name+str(confidence_out)+'.jpg',frame) 
+
+def save_Data():    #Outputs face detection data to text file
+    lines = [str(nTime), name + ': ' + str(confidence_out)]
+    with open('test_data.txt', 'a') as f:
+        for line in lines:
+            f.write(line)
+            f.write('\n')
+
+def frame_Visuals():
+    cv2.rectangle(frame, (0, 0), (100 + 150, 10 + 10), (19, 155, 35), cv2.FILLED) #Add box behind text for visibility
+    cv2.putText(frame,                                                            #Displays FPS 
+                f'FPS:{fps}',
+                (5, 15), 
+                font, 0.5, 
+                (255, 255, 255), 
+                2, 
+                2)
+    cv2.putText(frame,                                                            #Displays number of faces
+                f'Number of faces: {numFaces}',
+                (70, 15), 
+                font, 0.5, 
+                (255, 255, 255), 
+                2, 
+                2)
+
+def face_Frame_Visuals():
+        cv2.rectangle(frame, (left, top), (right, bottom), (19, 155, 35), 2)                 #Displays frame around detected face
+        cv2.rectangle(frame, (left, bottom -15), (right, bottom), (19, 155, 35), cv2.FILLED) #Displays box for name visibility
+        cv2.putText(frame, name, (left +3, bottom -3), font, 0.5, (255, 255, 255), 1)        #Displays name
+        cv2.putText(frame,f'{confidence}', (left +3, top -6), font, 0.5, (255, 255, 255), 1) #Put confidence interval above frame, split string to display as percentage. 
+
 belle = fr.load_image_file("dataset/belle_frontal_brow.jpg", mode='RGB') #Load image, convert to RGB on import
-#face encoding
 belleFaceEncoding = fr.face_encodings(belle)[0]
 
 Ike = fr.load_image_file("dataset/Ike_frontal_brow.jpg", mode='RGB') 
-ikeFaceEncoding = fr.face_encodings(Ike,)[0]
+ikeFaceEncoding = fr.face_encodings(Ike)[0]
 
-known_faces_encodings= [belleFaceEncoding, ikeFaceEncoding]
-known_face_names = ["belle", "Ike"]
+Test_face = fr.load_image_file("dataset/human.jpg", mode='RGB') 
+testFaceEncoding = fr.face_encodings(Test_face)[0]
 
-#detector = fr.get_frontal_face_detector() #detects the coordinate of faces
+known_faces_encodings= [belleFaceEncoding, ikeFaceEncoding, testFaceEncoding]
+known_face_names = ["belle", "Ike", "Test_face"]
 
 while True: #Loop to start taking all the frameworks from the camera
     ret, frame = webcam.read()
@@ -37,60 +69,32 @@ while True: #Loop to start taking all the frameworks from the camera
     face_locations = fr.face_locations(rgb_frame, model="hog")                  #check where faces are in the frame, uses hog model (faster but less accurate)
     face_encodings = fr.face_encodings(rgb_frame, face_locations, model=small)  #detects which faces are in the frame
     numFaces = len(face_encodings)                                              #Number of faces in frame = length of face_encodings array
-    confidence = str(fr.face_encodings)                                         #Confidence estimate is from euclidean distance from each comparison face 
 
     ctime = time.time() #Method to get fps by getting passed time since beginning and end of each loop
     fps= int(1/(ctime-ptime))
     ptime = ctime
 
-    cv2.rectangle(frame, (0, 0), (100 + 150, 10 + 10), (19, 155, 35), cv2.FILLED) #Add box behind text for visibility
-
-    cv2.putText(frame,
-                f'FPS:{fps}',
-                (5, 15), 
-                font, 0.5, 
-                (255, 255, 255), 
-                2, 
-                2)
-
-    cv2.putText(frame, 
-                f'Number of faces: {numFaces}',
-                (70, 15), 
-                font, 0.5, 
-                (255, 255, 255), 
-                2, 
-                2)
+    frame_Visuals()
 
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
 
         nTime = datetime.datetime.now().time()
-        matches = fr.compare_faces(known_faces_encodings, face_encoding, tolerance=0.6)
-
+        matches = fr.compare_faces(known_faces_encodings, face_encoding, tolerance=0.5)
         name = "Unknown"
 
         face_distances = fr.face_distance(known_faces_encodings, face_encoding) #Compares face encodings and tells you how similar the faces are
-        confidence = str(face_distances) #Confidence = face_distance of detected face and face_encoding
-        best_match_index = np.argmin(face_distances) #Most similar face_distance = the best match
+        best_match_index = np.argmin(face_distances)                            #Most similar face_distance = the best match
+
+        confidence = min(face_distances)                                        #Confidence = minimum distance returned by face_distance list
+        confidence_out = str(confidence)
 
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
         
-        #Draw rectangles
-        cv2.rectangle(frame, (left, top), (right, bottom), (19, 155, 35), 2)
-        cv2.rectangle(frame, (left, bottom -15), (right, bottom), (19, 155, 35), cv2.FILLED)
+        face_Frame_Visuals()
+        save_Face()
+        save_Data()
 
-        #Display Text
-        cv2.putText(frame, name, (left +3, bottom -3), font, 0.5, (255, 255, 255), 1)
-        cv2.putText(frame,f'{confidence}', (left +3, top -6), font, 0.5, (255, 255, 255), 1) #Put confidence interval above frame, split string to display as percentage. 
-        
-        lines = [str(nTime), name + ': ' + confidence]
-        with open('test_data.txt', 'a') as f:
-            for line in lines:
-                f.write(line)
-                f.write('\n')
-
-        cv2.imwrite("live_dataset/"+name+confidence+'.jpg',frame) #Each time face is detected, save image with name and confidence level
-        
     cv2.imshow('webcam', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
