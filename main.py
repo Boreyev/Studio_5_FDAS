d48f5e2feee2c09f5d34c9d9c2a17694065e3d5c
unknown <CALLIT1@student.op.ac.nz>
2022-05-08 16:47:12 +1200

    Added function to only save 5 images per face, overwrites images based on for loop

--- main.py
+++ main.py
@@ -14,12 +14,26 @@ def make_480p():    #Adjusts the camera input to 480p, saves resources. CANNOT B
     webcam.set(4, 480)
 
 def save_Face():    #Each time face is detected, save image with name and confidence level
-        height = bottom - top   #Define height / width
-        width = right - left
-        crop_Face = frame[top:top + height, left:left + width]  #Create new frame, use location encodings to crop face. 
-        save_Image = cv2.imwrite("live_dataset/"+name+"/"+name+str(confidence_out)+'.jpg',crop_Face) 
-        return save_Image
+    for i in range(5):
+        if i==5:
+            i = 0
+        elif i == 0 or 1 or 2 or 3 or 4:
+            height = bottom - top + 15   #Define height / width
+            width = right - left
+            crop_Face = frame[top:top + height, left:left + width]  #Create new frame, use location encodings to crop face. 
+            save_Image = cv2.imwrite("live_dataset/"+name+"/"+name+str(i)+'.jpg',crop_Face) 
 
+    return save_Image
+
+def resize_Face(): #Not in use as of now, work in progress. 
+    img_Height = 150
+    img_Width = 150
+    img_Dim = img_Width, img_Height
+    img_Name = cv2.imread("live_dataset/"+name+"/"+name+str(confidence_out)+'.jpg', cv2.IMREAD_UNCHANGED)
+    resize_Img = cv2.resize(img_Name, img_Dim)
+    save_Img_Resize = cv2.imwrite(img_Name, resize_Img)
+    return save_Img_Resize
+    
 def save_Data():    #Outputs face detection data to text file
     lines = [str(nTime), name + ': ' + str(confidence_out)]
     with open('test_data.txt', 'a') as f:
@@ -48,7 +62,7 @@ def face_Frame_Visuals():
         cv2.rectangle(frame, (left, top), (right, bottom), (19, 155, 35), 2)                 #Displays frame around detected face
         cv2.rectangle(frame, (left, bottom -15), (right, bottom), (19, 155, 35), cv2.FILLED) #Displays box for name visibility
         cv2.putText(frame, name, (left +3, bottom -3), font, 0.5, (255, 255, 255), 1)        #Displays name
-        cv2.putText(frame,f'{confidence}', (left +3, top -6), font, 0.5, (255, 255, 255), 1) #Put confidence interval above frame, split string to display as percentage. 
+        cv2.putText(frame,f'{confidence}', (left +3, bottom +8), font, 0.3, (255, 255, 255), 1) #Put confidence interval above frame, split string to display as percentage. 
 
 belle = fr.load_image_file("dataset/belle_frontal_brow.jpg", mode='RGB') #Load image, convert to RGB on import
 belleFaceEncoding = fr.face_encodings(belle)[0]
@@ -68,7 +82,7 @@ while True: #Loop to start taking all the frameworks from the camera
     frame_resize = cv2.resize(frame, (0, 0), fx=1, fy=1)    #Resizes frame by adjusting frame height and width.
                                                                 #Note: Reduced frame scale results in faster frames but lower detection accuracy.  
                                                                 #This method is left at the default 1, It can be upscaled but is not recommended. 
-    rgb_frame = frame_resize[:, :, ::1]                     #convertframe to rgb
+    rgb_frame = frame_resize[:, :, ::-1]                     #convertframe to rgb
 
     face_locations = fr.face_locations(rgb_frame, model="hog")                  #check where faces are in the frame, uses hog model (faster but less accurate)
     face_encodings = fr.face_encodings(rgb_frame, face_locations, model=small)  #detects which faces are in the frame
